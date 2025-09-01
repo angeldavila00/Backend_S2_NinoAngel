17df931896b6d5c1c6a1df0827a283a66c1238d6
angeldavila00 <angeldavila0975@gmail.com>
2025-08-31 23:47:30 -0500

    feat: :sparkles: prior research

---
+++ orientadaObjetos/objetos.js
@@ -0,0 +1,48 @@
+let nombres = { 
+    nombre: "Juan",
+    apellido: "Perez"
+};
+
+console.log(nombres.nombre);
+console.log(nombres.hasOwnProperty("segNombre"));
+//Resultado esperado:
+//Juan
+//False
+
+console.log(nombres);
+
+/*Modificando la propiedad <prototype>
+Esta propiedad se puede modificar, indicando explícitamente que debería referirse a otro prototipo. Se utilizan los siguientes métodos para lograr esto: */
+
+function perro (nombre,edad) {
+    let perro = Object.create(ObjectConstructor);
+    perro.nombre = nombre;
+    perro.edad = edad;
+
+    return perro;
+}
+
+let ObjectConstructor = {
+    habla: function(){
+        return "¡Soy un perro!"
+    }
+}
+
+let firulais = perro("Firulais", 9);
+
+console.log(firulais);
+
+// Class
+class animales {
+    constructor(nombre, especie) {
+        this.nombre = nombre;
+        this.especie = especie;
+    }
+    habla() {
+        return "¡Soy un perro!";
+    }
+}
+
+
+let bongo = new animales ("Bongo", "Peludo");
+console.log(bongo)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Programacion orientada a Objetos con javaScripts
+
+- Según la documentación de Mozilla:
+
+###### "Un lenguaje basado en prototipos toma el concepto de objeto prototípico, un objeto que se utiliza como una plantilla a partir de la cual se obtiene el conjunto inicial de propiedades de un nuevo objeto."
